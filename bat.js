1a694a12a62b557a91a8c7152689eb1d096417f0
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2021-12-01 09:56:23 +0000

    stash

--- bat.js
+++ bat.js
@@ -1,25 +1,49 @@
-const promiseYouWillWorkCallback = (shout, cb) => {
-  setTimeout(() => {
-    if (!shout) {
-      return cb(new Error("No shout passed!!"));
-    }
-    return cb(null, shout);
-  }, 2000);
-};
+// const promiseYouWillWorkCallback = (shout, cb) => {
+//   setTimeout(() => {
+//     if (!shout) {
+//       return cb(new Error("No shout passed!!"));
+//     }
+//     return cb(null, shout);
+//   }, 2000);
+// };
 
-const logShout = (err, shout) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`I shout ${shout}`);
-  }
-};
+// const logShout = (err, shout) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(`I shout ${shout}`);
+//   }
+// };
 
-promiseYouWillWorkCallback(null, logShout);
+// promiseYouWillWorkCallback(null, logShout);
 
 /**********************************/
 const promiseYouWillWorkPromise = (shout) => {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {}, 2000);
+    setTimeout(() => {
+      if (!shout) {
+        return reject(new Error("No shout!"));
+      }
+      return resolve(shout);
+    }, 2000);
   });
 };
+
+// promiseYouWillWorkPromise("For the Alliance!")
+//   .then((shout) => {
+//     console.log(`I shout ${shout}`);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const thisIsAwait = async (shout) => {
+  const theShout = await promiseYouWillWorkPromise(shout);
+  try {
+    console.log(theShout);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+thisIsAwait();
